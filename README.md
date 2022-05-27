a3d1ad44243626b79dba50947a223a475054f69d
Eduardo "mrtheduts" Souza <eduardomoreirafds@gmail.com>
2022-05-27 00:24:03 -0300

    Team list updated and added main game node

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ Repository of our submission for [Go Godot Jam 3](https://itch.io/jam/go-godot-j
 
 ---
 **Team:**
+- [@anarequena](https://github.com/anarequena)
+- [@Dipho](https://github.com/Dipho)
+- [@gufernandez](https://github.com/gufernandez)
+- [@JSpuri](https://github.com/JSpuri)
+- [@jpsousa78](https://github.com/jpsousa78)
+- [@lcbcFoo](https://github.com/lcbcFoo)
 - [@mrtheduts](https://github.com/mrtheduts)
+- [@pedrobc002](https://github.com/pedrobc002)
+- [@VFerrari](https://github.com/VFerrari)
---
+++ src/Main/Main.gd
@@ -0,0 +1,21 @@
+#
+# Main.gd - Main node's script (every other node will be below it in the game tree)
+#
+
+# Inherits Node's behavior
+extends Node
+
+# Declare custom signals this way:
+# signal example_signal
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
